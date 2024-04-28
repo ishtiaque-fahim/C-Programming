fc2cdd44148062f57f57486f872922e1ebc92e40
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-04-28 20:01:37 +0600

    Even Numbers
    Write a program that prints all even numbers between 1 and 100, including them if it is the case.

---
+++ Even Numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main()
+{
+    int e;
+    e = 2;
+    while (e <= 100)
+    {
+      printf("%d\n", e);
+      e += 2;
+
+    }
+
+    {
+        return 0;
+    }
+    }
+
+
+
