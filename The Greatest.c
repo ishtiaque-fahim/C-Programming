612c4535c5ea5e8503e3482a68fb864ad225a44d
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-04-28 19:23:02 +0600

    The Greatest
    Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior".

---
+++ The Greatest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c, x, y;
+    scanf("%d%d%d\n", &a, &b, &c);
+    x = (a + b + abs(a-b)) / 2;
+    y = (x + c + abs(x-c)) / 2;
+    printf("%d eh o maior\n", y);
+
+    return 0;
+}
