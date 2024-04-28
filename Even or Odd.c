7484678b74a336fc2423e4a5080051b82fb7a82c
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-04-28 20:09:18 +0600

    Even or Odd
    Read an integer value N. After, read these N values and print a message for each value saying if this value is odd, even, positive or negative. In case of zero (0), although the correct description would be "EVEN NULL", because by definition zero is even, your program must print only "NULL", without quotes.

---
+++ Even or Odd.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, x, i;
+    scanf("%d", &n);
+    i = 1;
+    while(i <= n)
+    {
+        scanf("%d", &x);
+        if(x % 2 == 0 && x > 0)
+        {
+            printf("EVEN POSITIVE\n");
+        }
+        else if(x % 2 == 0 && x < 0)
+        {
+            printf("EVEN NEGATIVE\n");
+        }
+        else if(x % 2 != 0 && x > 0)
+        {
+            printf("ODD POSITIVE\n");
+        }
+        else if(x % 2 != 0 && x < 0)
+        {
+            printf("ODD NEGATIVE\n");
+        }
+        else if(x == 0)
+        {
+            printf("NULL\n");
+        }
+        i++;
+
+
+    }
+
+    return 0;
+}
