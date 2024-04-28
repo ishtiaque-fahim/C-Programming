c8960e2d5c7fc5005974a565a4f5a621be43a93d
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-04-28 20:44:09 +0600

    Area of a Circle 2
    Given a number R calculate the area of a circle using the following formula:
    
    Area = π * R2.
    
    Note: consider π = 3.141592653

---
+++ Area of a Circle 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.141592653
+
+int main() {
+    double R, A;
+    scanf("%lf", &R);
+    A = PI * R * R;
+    printf("%.9lf", A);
+
+
+
+
+    return 0;
+}
